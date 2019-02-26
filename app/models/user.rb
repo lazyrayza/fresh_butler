8b1bae0a9428abeb728ed906d037df1032765424
Ricardo Vieira <ricmvieira@gmail.com>
2019-02-26 16:21:21 +0000

    Working on signup forms

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,11 @@
 class User < ApplicationRecord
+  USER_TYPE = %(Butler Client)
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :butler_bookings, class_name: :Booking, foreign_key: "butler_id", dependent: :destroy
   has_many :client_bookings, class_name: :Booking, foreign_key: "client_id", dependent: :destroy
-  # validates :first_name, presence: true
-  # validates :last_name, presence: true
-  # validates :address, presence: true
-  # validates :phone_number, presence: true
-  # validates :butler, presence: true
+
+  validates :user_type, presence: true, inclusion: { in: USER_TYPE }
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
