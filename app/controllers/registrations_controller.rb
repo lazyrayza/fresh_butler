242843b8db7b8839307e9249f3a0d68e0aa64686
Ricardo Vieira <ricmvieira@gmail.com>
2019-02-28 15:05:50 +0000

    Butler boolean working

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,8 +1,20 @@
 class RegistrationsController < Devise::RegistrationsController
+
+  def create
+    super
+    if params[:user][:butler] == "true"
+      @user.butler = true
+    else
+      @user.butler = false
+    end
+    @user.save
+  end
+
   private
 
   def after_update_path_for(*)
     flash[:notice] = "Account succesfully updated"
     profile_path
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-    devise_for :users, :controllers => { :registrations => :registrations }
+  devise_for :users, :controllers => { :registrations => :registrations }
   resources :users do
     resources :bookings, only: [:new, :create]
   end
