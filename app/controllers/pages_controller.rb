21205db8fe73f8215ff82afa075b0b70a5ba6c5d
Ricardo Vieira <ricmvieira@gmail.com>
2019-02-28 12:11:07 +0000

    Redirected user profile updates back to profile.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,11 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
-  def home
-  end
-
   def profile
     @user = current_user
   end
+
+  def home
+  end
+
 end
